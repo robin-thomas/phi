084bcb7a2b970cfe7c8819f9e2f54ec6b5b718da
Robin Thomas <robinthomas2591@gmail.com>
2022-02-01 10:11:38 +0800

    create new logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Orbitron&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
