28d47048993f824d1dc878d2136a9aa2c6aa4657
Robin Thomas <robinthomas2591@gmail.com>
2022-02-12 11:24:41 +0800

    fix failing test

--- tests/modules/chatbox/components/Emoji.test.js
+++ tests/modules/chatbox/components/Emoji.test.js
@@ -1,22 +1,15 @@
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import { Emoji } from '@/modules/chatbox/components';
 
 describe('Emoji', () => {
-  xit('Emoji Picker should be rendered', async () => {
-    const setEmoji = jest.fn();
-    render(<Emoji setEmoji={setEmoji}/>);
-
-    await waitFor(() => expect(screen.getByRole('button', { name: '😀' })).toBeInTheDocument());
-  });
-
-  xit('Verify that the setEmoji prop works fine', async () => {
+  it('Verify that the setEmoji prop works fine', async () => {
     const setEmoji = jest.fn();
     render(<Emoji setEmoji={setEmoji} />);
 
-    await waitFor(() => expect(screen.getByRole('button', { name: '😀' })).toBeInTheDocument());
+    const emoji = await screen.findByRole('button', { name: '😀' });
+    expect(emoji).toBeInTheDocument();
 
-    const emoji = screen.getByRole('button', { name: '😀' });
     fireEvent.click(emoji);
 
     expect(setEmoji).toHaveBeenCalledWith(expect.objectContaining({ emoji: '😀' }));
